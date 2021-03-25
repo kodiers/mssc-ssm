5aec03ff824f67ae5c713c7f80251b195234f64c
Victor Yamchinov <kodiers@gmail.com>
2021-03-26 00:50:28 +0300

    Initial commit

---
+++ src/test/java/com/tfl/msscssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfl.msscssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
